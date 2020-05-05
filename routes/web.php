db3702840cda6caca4987f8b91e13602d472be6c
shapo <shapo@debian.shapo>
2020-05-05 05:51:57 +0500

    add layouts in the index.blade.php and create CategoryController

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function (){
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
-Route::resource('rest', 'RestTestController')->names('restTest');  
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//> админка блога
+$groupdata = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog'
+];
+Route::group($groupdata, function (){
+    $methods = ['index', 'edit', 'store', 'update', 'create'];
+    Route::resource('categories', 'CategoryController')
+    ->only($methods)
+    ->names('blog.admin.categories');
+    
+});
+//<
+
+//Route::resource('rest', 'RestTestController')->names('restTest');  
+
+
+
